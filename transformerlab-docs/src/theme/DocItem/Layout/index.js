e4a33e73aa5cdae524cdc460ecdbf3cfbf735c5e
deep1401 <gandhi0869@gmail.com>
2026-05-01 10:33:10 -0600

    delete

--- transformerlab-docs/src/theme/DocItem/Layout/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import DocItemLayout from '@theme-original/DocItem/Layout';
-
-export default function DocItemLayoutWrapper(props) {
-  return <DocItemLayout {...props} />;
-}
